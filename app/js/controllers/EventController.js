9714f101a957152e8b04f657c8dcd5bf3a0df769
santiaago <santiago.ariassar@gmail.com>
2013-06-05 14:57:21 +0200

    data and event in angular

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -11,7 +11,40 @@ eventsApp.controller('EventController',
 				 city: 'Mount Doom',
 				 province: 'CA'
 			     },
-			     imageUrl: 'img/angularjs-logo.png'
+			     imageUrl: 'img/angularjs-logo.png',
+			     sessions: [
+				 {
+				     name: 'Directives Masterclass',
+				     creatorName: 'Bob this',
+				     duration: '1 hr',
+				     level: 'Advanced',
+				     abstract: 'in this session you will learn about directives',
+				     upVoteCount: 0
+
+				 },
+				 {
+				     name: 'Scopes for fun and profit',
+				     creatorName: 'Will that',
+				     duration: '3 hr',
+				     level: 'Intermediate',
+				     abstract: 'in this session you will learn about scopes',
+				     upVoteCount: 0
+				 },
+				 {
+				     name: 'Well Behaved Controllers',
+				     creatorName: 'Ang Le',
+				     duration: '2 hr',
+				     level: 'Introductory',
+				     abstract: 'in this session you will learn about scopes',
+				     upVoteCount:0
+				 }
+			     ]
 			 }
+			 $scope.upVoteSession = function(session){
+			     session.upVoteCount++;
+			 };
+			 $scope.downVoteSession = function(session){
+			     session.upVoteCount--;
+			 };
 		     }
 );
