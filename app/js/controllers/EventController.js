5b350b64f68c11090870d03c2b5e3cc401749aba
santiaago <santiago.ariassar@gmail.com>
2013-06-14 15:01:53 +0200

    first angular service

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,51 +1,14 @@
 'use strict';
 
 eventsApp.controller('EventController',
-		     function EventController($scope){
+		     function EventController($scope, eventData){
 			 $scope.snippet = '<span style="color:red">hi there</span>';
 			 $scope.boolValue = true;
 			 $scope.mystyle = {color:'red'};
 			 $scope.myclass = "blue";
 			 $scope.buttonDisabled = true;
 			 $scope.sortorder = 'name';//dash is for descendingupVoteCount';
-			 $scope.event = {
-			     name: 'Tree Boot Camp',
-			     date: 1359781015626,
-			     time: '10:30 am',
-			     location:{
-				 address: 'Gollum Headquarters',
-				 city: 'Mount Doom',
-				 province: 'CA'
-			     },
-			     imageUrl: 'img/angularjs-logo.png',
-			     sessions: [
-				 {
-				     name: 'Directives Masterclass',
-				     creatorName: 'Bob this',
-				     duration: 1,
-				     level: 'Advanced',
-				     abstract: 'in this session you will learn about directives',
-				     upVoteCount: 0
-
-				 },
-				 {
-				     name: 'Scopes for fun and profit',
-				     creatorName: 'Will that',
-				     duration: 2,
-				     level: 'Intermediate',
-				     abstract: 'in this session you will learn about scopes',
-				     upVoteCount: 0
-				 },
-				 {
-				     name: 'Well Behaved Controllers',
-				     creatorName: 'Ang Le',
-				     duration: 4,
-				     level: 'Introductory',
-				     abstract: 'in this session you will learn about scopes',
-				     upVoteCount:0
-				 }
-			     ]
-			 }
+			 $scope.event = eventData.event;
 			 $scope.upVoteSession = function(session){
 			     session.upVoteCount++;
 			 };
