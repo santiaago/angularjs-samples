e090b908f47dd69a21ff7232a095d57cf6d16b0c
santiaago <santiago.ariassar@gmail.com>
2013-06-14 14:44:57 +0200

    validation

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -2,8 +2,11 @@
 
 eventsApp.controller('EditEventController',
 		     function EditEventController($scope){
-			 $scope.saveEvent = function(event){
-			     window.alert('event ' + event.name + ' save');
+			 $scope.saveEvent = function(event, newEventForm){
+			     console.log(newEventForm);
+			     if(newEventForm.$valid){
+				 window.alert('event ' + event.name + ' save');
+			     }
 			 }
 			 $scope.cancelEdit = function(){
 			     window.location = "/EventDetails.html";
