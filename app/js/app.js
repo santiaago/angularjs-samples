241ea0b565ce83f96342c28be394520a61089907
santiaago <santiago.ariassar@gmail.com>
2013-06-19 18:34:19 +0200

    service

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var eventsApp = angular.module('eventsApp', ['ngSanitize','ngResource'])
+var eventsApp = angular.module('eventsApp', ['ngSanitize','ngResource', 'ngCookies'])
     .factory('myCache', function($cacheFactory){
 	return $cacheFactory('myCache', {capacity:3})
 	});
