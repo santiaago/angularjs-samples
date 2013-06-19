039376cb7a0ff2a75fdcd31778c6930c231189a8
santiaago <santiago.ariassar@gmail.com>
2013-06-19 13:58:33 +0200

    function

--- app/js/controllers/CompileSampleController.js
+++ app/js/controllers/CompileSampleController.js
@@ -1,7 +1,28 @@
  'use strict';
 
 eventsApp.controller('CompileSampleController',
-		     function CompileSampleController($scope, $compile){
+		     function CompileSampleController($scope, $compile, $parse){
+
+			 console.log('parse demo');
+			 //-1
+			 var fn = $parse('1+2');
+			 console.log(fn());
+			 //-2
+			 var getter = $parse('event.name');
+
+			 var context1 = {event: {name: 'AngularJS Boot Camp'}};
+			 var context2 = {event: {name: 'Code Camp'}};
+
+			 console.log(getter(context1));
+			 console.log(getter(context2));
+			 //-3
+			 console.log(getter(context2,context1));
+			 //-4
+			 var setter = getter.assign;
+			 setter(context2, 'Code Retreat');
+			 console.log(context2.event.name);
+
+			 console.log('parse demo done!')
 
 			 $scope.appendDivToElement = function(markup){
 			     return $compile(markup)($scope).appendTo(angular.element("#appendHere"));
