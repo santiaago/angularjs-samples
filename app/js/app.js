c0238c5768d71e71f40f1962422c4a57b2fc80ae
santiaago <santiago.ariassar@gmail.com>
2013-06-20 13:57:24 +0200

    Adding first route

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,13 @@
 'use strict';
 
 var eventsApp = angular.module('eventsApp', ['ngSanitize','ngResource', 'ngCookies'])
+    .config(function($routeProvider){
+	$routeProvider.when('/newEvent',
+			    {
+				templateUrl: 'templates/NewEvent.html',
+				controller: 'EditEventController'
+			    })
+    })
     .factory('myCache', function($cacheFactory){
 	return $cacheFactory('myCache', {capacity:3})
 	});
