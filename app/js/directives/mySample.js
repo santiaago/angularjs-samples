f1d8832295f6c03ad207f4783a4eae787bb7f632
santiaago <santiago.ariassar@gmail.com>
2013-06-28 17:49:28 +0200

    domain specific directives

--- app/js/directives/mySample.js
+++ app/js/directives/mySample.js
@@ -5,10 +5,13 @@ eventsApp.directive('mySample', function($compile){
     return {
 	//restrict: 'E',
 	//template: "<input type='text' ng-model='sampleData' /> {{sampleData}}</br>";
-	link: function(scope, element, attrs, controller){
-	    console.log('directive link function');
-	    var markup = "<input type='text' ng-model='sampleData' /> {{sampleData}}</br>";
-	    angular.element(element).html($compile(markup)(scope));
-	}
+	//demo simple directive
+	// link: function(scope, element, attrs, controller){
+	//     console.log('directive link function');
+	//     var markup = "<input type='text' ng-model='sampleData' /> {{sampleData}}</br>";
+	//     angular.element(element).html($compile(markup)(scope));
+	// }
+	restrict: 'C',
+	template: "<input type='text' ng-model='sampleData' /> {{sampleData}}</br>"
     };
 });
